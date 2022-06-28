efcd4b9c1d5c0cf4cec5e814d5b9cca346530eac
Dima <d2.grinevich@gmail.com>
2022-06-28 14:44:25 +0300

    added p

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ function App() {
             <div>dev/login branch commit 2</div>
             <div>pop-up/1</div>
             <div>pop-up/2</div>
+            <p>1</p>
+            <p>2</p>
         </div>
     );
 }
