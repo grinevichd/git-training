cbed1cc32633df208642b58010ef53d742e49cb3
Dima <d2.grinevich@gmail.com>
2022-06-28 14:33:42 +0300

    dev/popup 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div>commit-1</div>
-      <div>commit-2</div>
-      <div>dev/login branch commit</div>
-      <div>dev/login branch commit 2</div>
-        <div>pop-up/1</div>
-        <div>pop-up/2</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>commit-1</div>
+            <div>commit-2</div>
+            <div>dev/login branch commit</div>
+            <div>dev/login branch commit 2</div>
+            <div>pop-up/1</div>
+            <div>pop-up/2</div>
+        </div>
+    );
 }
 
 export default App;
